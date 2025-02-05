d2b1e5910f132e17a74386d25b0be6dcf10a2d8d
TanailyAb <L21100152@nlaredo.tecnm.mx>
2025-02-04 19:22:03 -0600

    cambios servidores

--- Parcial 1/ServerExpress/index.js
+++ Parcial 1/ServerExpress/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const cors = require('cors');
-
+//const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
-app.get('/data', (req, res,next) => {
-    res.sendFile( __dirname+ '/public/index.htm');
+app.get('/',(req, res,next)=>{
+    res.sendFile( __dirname+ '/public/index.html');
 });
-app.post('/data', (req, res) => {
+app.post('/',(req, res)=>{
     res.send('Hello World');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,()=>{
     console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
 });
