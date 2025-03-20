a413221a4c7bd837a32d336267818d1089473969
TanailyAb <L21100152@nlaredo.tecnm.mx>
2025-03-20 09:54:07 -0500

    Cambios

--- Parcial 2/Bearer/index.js
+++ Parcial 2/Bearer/index.js
@@ -1,1 +1,23 @@
-/*imple*/
+const express = require('express');
+const bearer = require('express-bearer-token');
+
+const app = express();
+const PORT = 3001;
+
+app.use(bearer());
+
+app.use(function (req, res, next){
+    if (req.token === 'tana'){
+        next();
+    } else {
+        res.status(401).send('No puedes pasar!');
+    }
+});
+
+app.get('/', (req, res)=>{
+    res.send("Hola mundo! Puedes pasar...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
+});
